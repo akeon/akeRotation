0938837be9d53e073744d4ac5020b9cc21dab120
akeon <akeon@azguard.net>
2014-10-14 12:42:50 +0800

    Update elemental.lua
    Added survival toggle (cleanse / auto windwalk totem)

--- shaman/elemental.lua
+++ shaman/elemental.lua
@@ -1,5 +1,6 @@
 PossiblyEngine.rotation.register_custom(262, "akeElementalShaman", {
 -- Author: AkeRotations
+-- Version: 1.4 14/10/14 Added survival toggle (cleanse / auto windwalk totem)
 -- Version: 1.3 14/10/14 Overhaul for 6.0.2. Add Unleash elements. Add Anscestral Swiftness. Revamp movement rotation in light of lightning bolt change.
 -- Version: 1.2 21/09/14 (MOP)
 -- Changelog: 1.2 Added earthquake.
@@ -23,17 +24,21 @@ PossiblyEngine.rotation.register_custom(262, "akeElementalShaman", {
 	{ "Healing Surge", { "toggle.selfheal", "player.health < 20" } },
 	{ "Healing Stream Totem", { "toggle.selfheal", "!player.totem(Healing Stream Totem)", "player.health < 30" } },
  
+ 	--Survival Abilities
+ 	{ "Windwalk Totem", { "toggle.survival", "!player.buff", "player.state.root" }, "player" }, 
+	{ "Windwalk Totem", { "toggle.survival", "!player.buff", "player.state.snare" }, "player" }, 
+	{ "Cleanse Spirit", { "toggle.survival", "!modifier.last(Cleanse)", "player.dispellable(Cleanse)" }, "player" }, 
+ 
 	-- Control Toggles
 	{ "Flame Shock", { "!modifier.multitarget", "mouseover.enemy", "mouseover.alive", "mouseover.deathin > 25", "mouseover.debuff(Flame Shock).duration <= 3", "toggle.mouseovers" }, "mouseover" },
 	{ "Earthquake", { "modifier.lcontrol" }, "ground" },
 
 	-- Cooldowns
-	{ "Stormlash Totem", { "modifier.cooldowns", "toggle.lash", "!totem(Stormlash Totem)" } },
 	{ "Fire Elemental Totem", "modifier.cooldowns" },
-	{ "Berserking", "modifier.cooldowns" },
 	{ "Elemental Mastery", "modifier.cooldowns" },
+	{ "Berserking", "modifier.cooldowns" },
 	{ "Ascendance", { "modifier.cooldowns", "!player.buff(Ascendance)" } },
-	{ "Spiritwalker's Grace", { "modifier.cooldowns", "player.buff(Ascendance)" } },
+	{ "Spiritwalker's Grace", { "player.moving", "modifier.cooldowns", "player.buff(Ascendance)" } },
 
 	-- Movement Rotation
 	{ "Flame Shock", { "player.moving", "target.debuff(Flame Shock).duration <= 3", "!player.buff(Spiritwalker's Grace)" } },
@@ -72,7 +77,7 @@ PossiblyEngine.rotation.register_custom(262, "akeElementalShaman", {
 },
 function()
 	PossiblyEngine.toggle.create('cleavemode', 'Interface\\Icons\\spell_nature_chainlightning', 'Cleave Mode', 'Enables cleave mode - Lower priority than multi-target.')
-	PossiblyEngine.toggle.create('lash', 'Interface\\Icons\\spell_frost_frostshock', 'Enable Stormlash', 'Beware if you have multiple shamans in the group.')
+	PossiblyEngine.toggle.create('survival', 'Interface\\Icons\\spell_frost_frostshock', 'Snare and Curse Removal', 'Enables automatic removal of roots, snares and curses.')
 	PossiblyEngine.toggle.create('selfheal', 'Interface\\Icons\\spell_nature_riptide', 'Enable Healing', 'Enable self healing logic')
 	PossiblyEngine.toggle.create('mouseovers', 'Interface\\Icons\\spell_fire_flameshock', 'Enable Mouseovers', 'Enable multi-target flameshock logic.')
 	PossiblyEngine.toggle.create('autotarget', 'Interface\\Icons\\ability_hunter_snipershot', 'Auto Target', 'Enable auto target nearest enemy logic.')
